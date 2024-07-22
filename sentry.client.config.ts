2753eda3b228529b2bff0037970732c4fafdd9b5
Avval Narang <avvalnnnnn@gmail.com>
2024-07-22 13:28:54 +0530

    Change Configuration to Vercel

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://5a9f523b873b96cb247f653a8061c6c8@o4507644123283456.ingest.de.sentry.io/4507644129443920",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
@@ -25,6 +25,10 @@ Sentry.init({
       // Additional Replay configuration goes in here, for example:
       maskAllText: true,
       blockAllMedia: true,
+    }),
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "dark",
     }),
   ],
 });
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://5a9f523b873b96cb247f653a8061c6c8@o4507644123283456.ingest.de.sentry.io/4507644129443920",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
